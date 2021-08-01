d2576a4686c6e1763bd0ef8bdf4b5982db408151
Super0Water <52554696+Super0Water@users.noreply.github.com>
2021-08-01 19:08:22 -0400

    update sql statement

--- Optimizing Public Transportation (Kafka)/consumers/ksql.py
+++ Optimizing Public Transportation (Kafka)/consumers/ksql.py
@@ -35,10 +35,9 @@ CREATE TABLE turnstile_summary
 WITH (VALUE_FORMAT = JSON) AS
     SELECT station_id INT,
     station_name VARCHAR,
-    line VARCHAR,
     COUNT(station_id) as count
     from turnstile
-    group by station_id;
+    group by station_id,station_name;
 """
 
 
